2c658efc2f06ffeab793294438b40dedcfe8dbd6
Vipin Yadav <vipiny35@gmail.com>
2023-09-29 22:03:59 +0530

    add unique user email constraints

--- src/prisma/seed.ts
+++ src/prisma/seed.ts
@@ -8,6 +8,13 @@ async function main() {
   const userData: Prisma.UserCreateInput = {
     name: 'Vipin',
     email: 'vipiny35@gmail.com',
+    Document: {
+      create: {
+        type: 'DRIVER_LICENSE',
+        filename: 'driver_license.pdf',
+        url: 'https://www.google.com',
+      },
+    }
   }
   await prisma.user.create({ data: userData });
 }
