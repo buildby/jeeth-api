81115b0ab8eb709f967f0acc6b16297d8d9a02e4
MaxSingh01 <98078159+MaxSingh01@users.noreply.github.com>
2023-10-26 19:35:29 +0530

    get all vendors endpoint added

--- src/controllers/VendorClient.controller.ts
+++ src/controllers/VendorClient.controller.ts
@@ -1,20 +1,36 @@
 import { RequestHandler } from "express";
 import * as VendorClientService from "../services/VendorClient.service";
 
-export const getVendorClientsbyVendorId: RequestHandler = async (req, res, next) => {
+export const getVendors: RequestHandler = async (_req, res, next) => {
   try {
-
-    const vendorId = +req.params.vendorId;
-
-    const vendorClients = await VendorClientService.getVendorByVendorId(vendorId);
+    const vendors = await VendorClientService.getVendors();
 
     return res.status(200).json({
-      result: 'success',
+      result: "success",
+      data: vendors,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getVendorClientsbyVendorId: RequestHandler = async (
+  req,
+  res,
+  next
+) => {
+  try {
+    const vendorId = +req.params.vendorId;
+
+    const vendorClients = await VendorClientService.getVendorByVendorId(
+      vendorId
+    );
+
+    return res.status(200).json({
+      result: "success",
       data: vendorClients,
     });
-
   } catch (error) {
     next(error);
   }
-
-}
+};
--- src/routes/VendorClient.route.ts
+++ src/routes/VendorClient.route.ts
@@ -3,4 +3,6 @@ import * as VendorClient from "../controllers/VendorClient.controller";
 
 export const VendorClientRoutes: Router = express.Router();
 
+VendorClientRoutes.get('/', VendorClient.getVendorClientsbyVendorId);
+
 VendorClientRoutes.get('/:id', VendorClient.getVendorClientsbyVendorId);
