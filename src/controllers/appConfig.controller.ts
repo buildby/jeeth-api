043b09ab6215be3dd3f9d5cfddaaf16022b5fe64
Darshan Tada <190540107203@darshan.ac.in>
2023-10-31 14:28:10 +0530

    app config schema changed

--- src/controllers/appConfig.controller.ts
+++ src/controllers/appConfig.controller.ts
@@ -1,102 +1,88 @@
-// import { AppConfig } from "aws-sdk";
-// import * as AppConfigService from "../services/appConfig.service";
-// import { Response, Request, NextFunction } from "express";
+import { AppConfig } from "aws-sdk";
+import * as AppConfigService from "../services/appConfig.service";
+import { Response, Request, NextFunction } from "express";
 
-// export const insertAppConfig = async (
-//   req: any,
-//   res: Response,
-//   next: NextFunction
-// ) => {
-//   if (req.body.type == "FAQ") {
-//     req.body.value = JSON.stringify(req.body.value);
-//   }
-//   let data = await AppConfigService.createAppConfig(req.body);
-//   if (data) {
-//     res.status(200).json({
-//       message: "Details added successfully",
-//       detail: data,
-//       type: true,
-//     });
-//   } else {
-//     res.status(500).json({
-//       message: "Error adding Details",
-//       type: false,
-//     });
-//   }
-// };
-// export const fetchAppConfig = async (
-//   req: any,
-//   res: Response,
-//   next: NextFunction
-// ) => {
-//   const config = await AppConfigService.findAppConfig({
-//     type: req.body.type,
-//   });
-
-//   res.status(200).json({
-//     result: config,
-//     success: true,
-//   });
+// Vechjile Make Appcionfig = {
+//     "type": "Vechile Make",
+//     value: '["Toyota","Maruti"]',
 // };
 
-// export const fetchCommonAppConfig = async (
-//   req: any,
-//   res: Response,
-//   next: NextFunction
-// ) => {
-//   const config = await AppConfigService.fetchCommonAppConfig({
-//     commonType: req.body.commonType,
-//   });
-
-//   res.status(200).json({
-//     result: config,
-//     success: true,
-//   });
+// Vechjile Model Appcionfig = {
+//     "type": "Vechile Models",
+//     value: '[{"make":"Toyota","modelName":"Fortuner"},{"make":"Maruti","modelName":"Swift"}]',
 // };
-// export const fetchAllRoutes = async (
-//   req: any,
-//   res: Response,
-//   next: NextFunction
-// ) => {
-//   const config = await AppConfigService.fetchAllRoutes({
-//     commonType: req.body.type,
-//   });
-//   let type: any = [];
-//   if (config.length > 0) {
-//     config.forEach((element: any) => {
-//       type.push(element['type']);
-//     });
-//   }
-//   res.status(200).json({
-//     result: config,
-//     type: type,
-//     success: true,
 
-//   });
-// };
+export const insertAppConfig = async (
+    req: any,
+    res: Response,
+    next: NextFunction
+) => {
+
+    let { type, value } = req.body;
+
+    value = JSON.stringify(value);
+
+    let data = await AppConfigService.createAppConfig({ type: type, value: value, });
+    if (data) {
+        res.status(200).json({
+            message: "Details added successfully",
+            detail: data,
+            type: true,
+        });
+    } else {
+        res.status(500).json({
+            message: "Error adding Details",
+            type: false,
+        });
+    }
+};
+export const fetchVehicleConfigs = async (
+    req: any,
+    res: Response,
+    next: NextFunction
+) => {
+
+    let { type, value } = req.body;
+
+    value = JSON.stringify(value);
+
+    let data = await AppConfigService.createAppConfig({ type: type, value: value, });
+
+    // const makes = await AppConfigService.fetchAppConfig('Vehicle Makes');
+    // const models = await AppConfigService.fetchAppConfig('Vehicle Models');
+
+
+    res.status(200).json({
+        result: 'success',
+        // data: { makes, models },
+    });
+};
+
+
+
 
 
 // export const getAppConfigs = async (
-//   req: any,
-//   res: Response,
-//   next: NextFunction
+//     req: any,
+//     res: Response,
+//     next: NextFunction
 // ) => {
 
-//   try {
-//     const { types } = req.body;
+//     try {
+//         const { types } = req.body;
 
-//     const configs = await AppConfigModel.find({
-//       type: { $in: types },
-//     });
+//         const configs = await AppConfigModel.find({
+//             type: { $in: types },
+//         });
 
-//     return res.status(200).json({
-//       result: configs,
-//       success: configs != null,
-//     });
+//         return res.status(200).json({
+//             result: configs,
+//             success: configs != null,
+//         });
 
-//   } catch (e) {
-//     return res.status(400).json({
-//       success: false,
-//     });
-//   }
+//     } catch (e) {
+//         return res.status(400).json({
+//             success: false,
+//         });
+//     }
 // };
--- src/services/appConfig.service.ts
+++ src/services/appConfig.service.ts
@@ -1,12 +1,12 @@
-// import { Prisma } from "@prisma/client";
-// import prisma from "../prisma/client";
+import { Prisma } from "@prisma/client";
+import prisma from "../prisma/client";
 
 
 
-// export const createAppConfig = (data: Prisma.AppConfigCreateInput) => prisma.appConfig.create({ data: data });
+export const createAppConfig = (data: Prisma.AppConfigCreateInput) => prisma.appConfig.create({ data: data });
 
 
-// export const fetchAppConfig = (type: String) => prisma.appConfig.findUnique({ where: { type: type } });
+export const fetchAppConfig = (type: string) => prisma.appConfig.findFirst({ where: { type: type } });
 
 // export const fetchCommonAppConfig = (commonType: string) => prisma.appConfig.findMany({ where: { commonType: commonType } });
 
